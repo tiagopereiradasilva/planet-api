0f4f30ac2480c43854e5a61c0ffee0f8e07b93c6
Tiago Silva <tiagopereiracx@gmail.com>
2024-05-08 23:32:49 -0300

    feat: Adiciona entidade Planet

---
+++ src/main/java/com/tiago/planetapi/domain/Planet.java
@@ -0,0 +1,21 @@
+package com.tiago.planetapi.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "planets")
+public class Planet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String climate;
+    private String terrain;
+}
