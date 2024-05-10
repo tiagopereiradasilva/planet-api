b8e0a7394e584f8f006dc33e93f94367374a276e
Tiago Silva <tiagopereiracx@gmail.com>
2024-05-09 23:19:40 -0300

    test: Adiciona constante para planeta utilizado nos testes

---
+++ src/test/java/com/tiago/planetapi/common/PlanetConstants.java
@@ -0,0 +1,7 @@
+package com.tiago.planetapi.common;
+
+import com.tiago.planetapi.domain.Planet;
+
+public class PlanetConstants {
+    public static final Planet PLANET = new Planet("planet", "climate", "terrain");
+}
