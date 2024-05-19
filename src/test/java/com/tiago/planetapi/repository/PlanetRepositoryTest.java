82d848bcabe2c2bdb9d15db049c2e591bb1d39ca
Tiago Silva <tiagopereiracx@gmail.com>
2024-05-19 00:41:15 -0300

    test: Adiciona teste para validar lançamento de exceptions  em caso de tentativa de salvar Planeta com dados inválidos

---
+++ src/test/java/com/tiago/planetapi/repository/PlanetRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.tiago.planetapi.repository;
+
+import static com.tiago.planetapi.common.PlanetConstants.EMPTY_PLANET;
+import static com.tiago.planetapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import com.tiago.planetapi.domain.Planet;
+
+// Anotação para utilizar banco em memória (com o H2) e aplicar o contexto do spring para o "PlanetRepository" ser executado devidamente
+@DataJpaTest
+public class PlanetRepositoryTest {
+    
+    @Autowired
+    private PlanetRepository repository;
+
+    @Autowired
+    private TestEntityManager entityManager; // Entidade para fazer buscas no banco e validar se a "integração" está funcionando, dessa forma o teste será validado.
+
+    @Test
+    public void savePlanet_WithValidData_ReturnsPlanet(){
+        Planet planet = repository.save(PLANET);
+        Planet actual = entityManager.find(Planet.class, planet.getId());
+        assertThat(actual).isNotNull();
+        assertThat(actual.getName()).isEqualTo(PLANET.getName());
+        assertThat(actual.getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(actual.getTerrain()).isEqualTo(PLANET.getTerrain());  
+    }
+
+    @Test
+    public void savePlanet_WithInvalidData_ThrowsException(){        
+        Planet invalidPlanet = new Planet();       
+        assertThatThrownBy(() -> repository.save(invalidPlanet));
+        assertThatThrownBy(() -> repository.save(EMPTY_PLANET));
+    }
+
+
+
+
+}
