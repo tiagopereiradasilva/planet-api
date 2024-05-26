dd784329fcd5dfda6467f431927374bdd3fca4ce
Tiago Silva <tiagopereiracx@gmail.com>
2024-05-25 22:48:32 -0300

    fix: Adiciona tratamento de exceção para quando tentamos criar Planeta já existente

--- src/main/java/com/tiago/planetapi/exception/GeneralExceptionHandler.java
+++ src/main/java/com/tiago/planetapi/exception/GeneralExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.tiago.planetapi.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -15,5 +18,11 @@ public class GeneralExceptionHandler extends ResponseEntityExceptionHandler{
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
             HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         return super.handleMethodArgumentNotValid(ex, headers, HttpStatus.UNPROCESSABLE_ENTITY, request);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    private ResponseEntity<ProblemDetail> handlerDataIntegrityViolationException(DataIntegrityViolationException ex){
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.CONFLICT);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(problemDetail);
     }
 }
